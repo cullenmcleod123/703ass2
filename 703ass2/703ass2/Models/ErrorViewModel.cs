00b45114cbb21710d8774f8573d00196b307c4c9
cullenmcleod <79872319+cullenmcleod123@users.noreply.github.com>
2024-11-07 14:18:50 +1300

    asp.net added

--- 703ass2/703ass2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _703ass2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
