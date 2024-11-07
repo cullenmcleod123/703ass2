5f525a4b3947e967dbaeec18ceabf73f3dc9d175
cullenmcleod <79872319+cullenmcleod123@users.noreply.github.com>
2024-11-07 13:25:57 +1300

    website added

---
+++ 703ass2/703ass2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _703ass2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
