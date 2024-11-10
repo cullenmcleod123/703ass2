5790a09271978ce6e4889a048a610c8bf0c62149
Ruben <rubenlyes@gmail.com>
2024-11-10 00:58:38 +0000

    added connection string

---
+++ 702assignment2/702assignment2/DatabaseAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace _702assignment2
+{
+    public class DataAccess
+    {
+        private readonly string _connectionString;
+
+        // Constructor to inject IConfiguration
+        public DataAccess(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("ServerConnection");
+        }
+
+        public void AddCustomer(string firstName, string lastName, string email, string phoneNumber, string address, int? companyId, int travelAgencyId)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("AddCustomer", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Add parameters
+                    cmd.Parameters.AddWithValue("@CustFirstName", firstName);
+                    cmd.Parameters.AddWithValue("@CustLastName", lastName);
+                    cmd.Parameters.AddWithValue("@CustEmail", email);
+                    cmd.Parameters.AddWithValue("@CustPhoneNumber", phoneNumber);
+                    cmd.Parameters.AddWithValue("@CustAddress", address);
+                    cmd.Parameters.AddWithValue("@CompanyID", (object)companyId ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@TravelAgencyID", travelAgencyId);
+
+                    // Open connection and execute the command
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+
+}
--- 702assignment2/702assignment2/obj/Debug/net8.0/702assignment2.AssemblyInfo.cs
+++ 702assignment2/702assignment2/obj/Debug/net8.0/702assignment2.AssemblyInfo.cs
@@ -11,12 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-_702assignment2-e88aa8d1-0970-4010-b1db-103c0559ea7d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("702assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945f2e44feff1327a996d1aca3f1e37d5567d9c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("702assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("702assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
