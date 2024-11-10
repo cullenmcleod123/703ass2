0e98758257d5a4a80388a7f197912d071ff5ef28
Ruben <rubenlyes@gmail.com>
2024-11-10 02:20:40 +0000

    .

--- IT703 - Assignment2/IT703 - Assignment2/Program.cs
+++ IT703 - Assignment2/IT703 - Assignment2/Program.cs
@@ -1,11 +1,14 @@
+using IT703___Assignment2;
 using IT703___Assignment2.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddTransient<DataAccess>();
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("ServerConnection") ?? throw new InvalidOperationException("Connection string 'ServerConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -15,6 +18,15 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dataAccess = services.GetRequiredService<DataAccess>();
+
+    // Add a customer (you can customize the values passed in)
+    dataAccess.AddCustomer("John", "Doe", "john.doe@example.com", "1234567890", "123 Elm Street", null, null);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- IT703 - Assignment2/IT703 - Assignment2/obj/Debug/net8.0/IT703 - Assignment2.AssemblyInfo.cs
+++ IT703 - Assignment2/IT703 - Assignment2/obj/Debug/net8.0/IT703 - Assignment2.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IT703 - Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e5f3dce31ccea7300a1ca169a4a0c5000747e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IT703 - Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IT703 - Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
